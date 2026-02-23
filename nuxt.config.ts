f2cf13ae953a19b3bc58c9df5131f203007e0849
Eric Carvalho da Silveira <eric.silveira@moneyp.com.br>
2026-02-23 08:10:38 -0300

    vue toast error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss'],
-  plugins: [
-    { src: '~/plugins/vue-toast', ssr: false }
-  ],
+  modules: ['@nuxtjs/tailwindcss']
 })
--- plugins/vue-toast.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue';
-import VueToast from 'vue-toast-notification';
-import 'vue-toast-notification/dist/theme-default.css';
-
-Vue.use(VueToast);
